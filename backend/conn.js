4d4ca1e0082c203655c28c37c433253dee7ff9bb
vipul <vipulgehlot43@gmail.com>
2026-03-10 20:38:37 +0530

    dd

--- backend/conn.js
+++ backend/conn.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 
-const uri =
-  import.meta.env.VITE_MONGO_URL || "mongodb://127.0.0.1:27017/usersDB";
-
-export const connectDB = async () => {
+export const connectDB = async (uri) => {
   try {
     await mongoose.connect(uri);
     console.log("MongoDB connected");
--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,27 @@ import { connectDB } from "./conn.js";
 import products from "./models/productModel.js";
 import loginSignupRoute from "./models/loginSignupModel.js";
 import cwRoutes from "./models/othModels.js";
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import dotenv from "dotenv";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Load .env from one level up (project root)
+dotenv.config({ path: resolve(__dirname, '../.env') });
+
+export const mongoUri = process.env.MONGO_URL || `mongodb://localhost:27017/usersDB`;
+
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-connectDB();
+connectDB(mongoUri);
 
 app.use("/product", products);
 
