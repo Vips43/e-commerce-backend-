35b13d1f7b5ec103f91859de6e2e6480b42cfb98
vipul <vipulgehlot43@gmail.com>
2026-03-13 22:11:30 +0530

    der

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { useAuthStore } from '../store/loginSignupStore'
 import ProductCard from '../components/ProductCard'
 import CategoryBar from '../components/CategoryBar'
 import ImageSlider from '../components/ImageSlider'
-import SliderContainer from '../components/sliderContainer'
+import SliderContainer from '../components/SliderContainer'
 
 function HomePage() {
   const getLoggedStatus = useAuthStore(state => state.getLoggedStatus)
